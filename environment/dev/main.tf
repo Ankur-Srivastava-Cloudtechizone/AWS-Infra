252ca6994bdd4532a3e87f7f54d3a3b1257ce5cb
Ankur Srivastava <ankur.ds.aws@gmail.com>
2025-06-16 15:34:06 +0530

    My First Commit

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -4,37 +4,37 @@ module "ec2" {
   instances = var.ec2_instances
 }
 
-module "aurora" {
-  source      = "../../modules/aurora"
-  db_clusters = var.db_clusters
-}
+# module "aurora" {
+#   source      = "../../modules/aurora"
+#   db_clusters = var.db_clusters
+# }
 
-module "eks" {
-  source    = "../../modules/eks"
-  clusters  = var.eks_clusters
-}
+# module "eks" {
+#   source    = "../../modules/eks"
+#   clusters  = var.eks_clusters
+# }
 
-module "ecr" {
-  source        = "../../modules/ecr"
-  repositories  = var.ecr_repositories
-}
+# module "ecr" {
+#   source        = "../../modules/ecr"
+#   repositories  = var.ecr_repositories
+# }
 
-module "cloudfront" {
-  source         = "../../modules/cloudfront"
-  distributions  = var.cloudfront_distributions
-}
+# module "cloudfront" {
+#   source         = "../../modules/cloudfront"
+#   distributions  = var.cloudfront_distributions
+# }
 
-module "route53" {
-  source  = "../../modules/route53"
-  records = var.route53_records
-}
+# module "route53" {
+#   source  = "../../modules/route53"
+#   records = var.route53_records
+# }
 
-module "s3_backup" {
-  source  = "../../modules/s3_backup"
-  buckets = var.s3_buckets
-}
+# module "s3_backup" {
+#   source  = "../../modules/s3_backup"
+#   buckets = var.s3_buckets
+# }
 
-module "etl" {
-  source  = "../../modules/etl"
-  jobs    = var.etl_jobs
-}
+# module "etl" {
+#   source  = "../../modules/etl"
+#   jobs    = var.etl_jobs
+# }
