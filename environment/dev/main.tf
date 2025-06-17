c20e587344e72455335c937ea8d103565cae044e
Ankur Srivastava <ankur.ds.aws@gmail.com>
2025-06-17 23:54:38 +0530

    My First Commit

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -14,51 +14,51 @@ module "ec2" {
 }
 
 
-module "aurora" {
-  source        = "../../modules/aurora"
-  db_clusters   = var.db_clusters
-  common_tags   = var.common_tags
-  environment   = var.environment
-}
+# module "aurora" {
+#   source        = "../../modules/aurora"
+#   db_clusters   = var.db_clusters
+#   common_tags   = var.common_tags
+#   environment   = var.environment
+# }
 
-module "eks" {
-  source        = "../../modules/eks"
-  eks_clusters  = var.eks_clusters
-  common_tags   = var.common_tags
-  environment   = var.environment
-}
+# module "eks" {
+#   source        = "../../modules/eks"
+#   eks_clusters  = var.eks_clusters
+#   common_tags   = var.common_tags
+#   environment   = var.environment
+# }
 
-module "ecr" {
-  source            = "../../modules/ecr"
-  ecr_repositories  = var.ecr_repositories
-  common_tags       = var.common_tags
-  environment       = var.environment
-}
+# module "ecr" {
+#   source            = "../../modules/ecr"
+#   ecr_repositories  = var.ecr_repositories
+#   common_tags       = var.common_tags
+#   environment       = var.environment
+# }
 
-module "cloudfront" {
-  source                  = "../../modules/cloudfront"
-  cloudfront_distributions = var.cloudfront_distributions
-  common_tags             = var.common_tags
-  environment             = var.environment
-}
+# module "cloudfront" {
+#   source                  = "../../modules/cloudfront"
+#   cloudfront_distributions = var.cloudfront_distributions
+#   common_tags             = var.common_tags
+#   environment             = var.environment
+# }
 
-module "route53" {
-  source           = "../../modules/route53"
-  route53_records  = var.route53_records
-  common_tags      = var.common_tags
-  environment      = var.environment
-}
+# module "route53" {
+#   source           = "../../modules/route53"
+#   route53_records  = var.route53_records
+#   common_tags      = var.common_tags
+#   environment      = var.environment
+# }
 
-module "s3_backup" {
-  source      = "../../modules/s3_backup"
-  s3_buckets  = var.s3_buckets
-  common_tags = var.common_tags
-  environment = var.environment
-}
+# module "s3_backup" {
+#   source      = "../../modules/s3_backup"
+#   s3_buckets  = var.s3_buckets
+#   common_tags = var.common_tags
+#   environment = var.environment
+# }
 
-module "etl" {
-  source      = "../../modules/etl"
-  etl_jobs    = var.etl_jobs
-  common_tags = var.common_tags
-  environment = var.environment
-}
+# module "etl" {
+#   source      = "../../modules/etl"
+#   etl_jobs    = var.etl_jobs
+#   common_tags = var.common_tags
+#   environment = var.environment
+# }
